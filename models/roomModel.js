d37fc2b1235ee2a6e67771d3bbd3c7e2d6e0ed08
Nzoputachi <samuelnzoputachi@gmail.com>
2024-03-26 04:51:08 +0100

    Modified gitignore and model.js

--- models/roomModel.js
+++ models/roomModel.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const roomSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     roomType: {
         type: mongoose.Schema.Types.ObjectId,
